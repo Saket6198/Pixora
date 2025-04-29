7e57cb030e76c99d69a2495384cf8e58f44b42fd
Saket Singh <137860573+Saket6198@users.noreply.github.com>
2025-04-29 23:36:49 +0530

    add liveblocks authentication route for user session management

---
+++ src/app/api/liveblocks-auth/route.ts
@@ -0,0 +1,32 @@
+import { Liveblocks } from "@liveblocks/node";
+import { auth } from "~/server/auth";
+import { db } from "~/server/db";
+import { env } from "~/env";
+const liveblocks = new Liveblocks({
+  secret: env.LIVEBLOCKS_SECRET_KEY ?? "",
+});
+
+export async function POST() {
+  const userSession = await auth();
+  const user = await db.user.findUnique({
+    where: {
+      id: userSession?.user?.id,
+    },
+  });
+  if (!user) {
+    return new Response("User not found", { status: 404 });
+  }
+  const session = liveblocks.prepareSession(user.id, {
+    userInfo: {
+      name: user?.email ?? "Anonymous",
+    },
+  });
+
+  session.allow(`room:${"test"}`, session.FULL_ACCESS);
+
+  const { status, body } = await session.authorize();
+  console.log("Session Status:", status);
+  console.log("Session Body:", body);
+
+  return new Response(body, { status });
+}
