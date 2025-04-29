c9117db0f928b81a1f22464d8adffc7e00026195
Saket Singh <137860573+Saket6198@users.noreply.github.com>
2025-04-29 23:38:47 +0530

    add Room component for managing collaborative sessions with Liveblocks

---
+++ src/components/liveblocks/room.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import type { ReactNode } from "react";
+import type React from "react";
+import {
+  ClientSideSuspense,
+  LiveblocksProvider,
+  RoomProvider,
+} from "@liveblocks/react";
+import { LiveList, LiveObject, LiveMap } from "@liveblocks/client";
+import type { Layer } from "~/types";
+
+export function Room({
+  children,
+  roomid,
+}: {
+  children: ReactNode;
+  roomid: string;
+}) {
+  return (
+    <>
+      <LiveblocksProvider authEndpoint="/api/liveblocks-auth">
+        <RoomProvider
+          id={roomid}
+          initialPresence={{
+            selection: [],
+            cursor: null,
+            penColor: null,
+            pencilDraft: null,
+          }}
+          initialStorage={{
+            roomColor: { r: 30, b: 30, g: 30 },
+            layers: new LiveMap<string, LiveObject<Layer>>(),
+            layerIds: new LiveList([]),
+          }}
+        >
+          <ClientSideSuspense
+        fallback={
+          <p className="flex min-h-screen flex-col items-center justify-center gap-3 font-semibold">
+            <img
+              className="h-10 w-10 animate-bounce"
+              src="/icons/canva.svg"
+            ></img>
+            Loading...
+          </p>
+        }
+      >
+        {children}
+      </ClientSideSuspense>
+        </RoomProvider>
+      </LiveblocksProvider>
+      
+    
+    </>
+  );
+}
