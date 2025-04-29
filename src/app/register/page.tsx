730a0101ed86210aa42e6178bb901849bfa88464
Saket Singh <137860573+Saket6198@users.noreply.github.com>
2025-04-29 23:38:07 +0530

    optimized register page

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,40 +1,3 @@
-// export default function Page() {
-//   return (
-//     <div className="flex min-h-screen items-center justify-center bg-white px-4">
-//       <div className="w-full max-w-sm flex flex-col gap-6 bg-red-500 text-center p-6 rounded-md">
-//         <p className="text-2xl font-bold">Sign Up</p>
-//         <form className="flex flex-col gap-6">
-//           <div className="relative">
-//             <label htmlFor="email" className="absolute top-0 left-1 px-1 text-sm">
-//               EMAIL
-//             </label>
-//             <input
-//               type="email"
-//               id="email"
-//               name="email"
-//               required
-//               className="rounded-sm border border-gray-300 bg-white px-3 py-3 text-black w-full"
-//             />
-//           </div>
-//           <div className="relative">
-//             <label htmlFor="password" className="absolute top-0 left-1 px-1 text-sm">
-//               PASSWORD
-//             </label>
-//             <input
-//               type="password"
-//               id="password"
-//               name="password"
-//               className="rounded-sm border border-gray-300 bg-white px-3 py-3 text-black w-full"
-//               required
-//             />
-//             <button
-//           </div>
-//         </form>
-//       </div>
-//     </div>
-//   );
-// }
-
 "use client";
 
 import type React from "react";
@@ -50,14 +13,27 @@ import {register} from "../actions/auth";
 import { AiFillDiscord } from "react-icons/ai";
 import { Loader2 } from "lucide-react";
 import {signIn as Oauth} from "next-auth/react";
-
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { useSession} from "next-auth/react";
 
 export default function Page() {
+    const router = useRouter();
+    const {data: session, status} = useSession();
   const [errorMessage, formAction, isPending] = useActionState(
     register,
     undefined,
   );
-
+  console.log("Session data:", session); 
+  const [dpending, setdpending] = useState(false);
+  const [gpending, setgpending] = useState(false);
+  useEffect(() => {
+    if(status === "authenticated"){
+      router.replace("/dashboard"); 
+    }
+  }, [status, router]);
+  
+  
   useEffect(() => {
     if (errorMessage) {
       toast.error(errorMessage, {
@@ -71,126 +47,162 @@ export default function Page() {
     }
   }, [errorMessage]);
 
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-50 px-4 py-12 dark:bg-zinc-900">
-      <ToastContainer />
-      <div className="shadow-input mx-auto w-full max-w-md rounded-2xl bg-white p-8 dark:bg-black">
-        <h2 className="text-2xl font-bold text-neutral-800 dark:text-neutral-200">
-          Create an account
-        </h2>
-        <p className="mt-2 max-w-sm text-sm text-neutral-600 dark:text-neutral-300">
-          Join us today and start your journey with our platform.
-        </p>
+  if(status === "loading"){
+    return <p className="min-h-screen items-center justify-center flex flex-col gap-3"><img className="h-10 w-10 animate-bounce" alt="pixora-icon" src="/icons/canva.svg"></img>Authenticating</p>
+  }
+  if(status === "unauthenticated"){
 
-        <form action={formAction} className="my-8">
-          <div className="mb-4 flex flex-col space-y-2 md:flex-row md:space-y-0 md:space-x-2">
-            <LabelInputContainer>
-              <Label htmlFor="firstname">First name</Label>
-              <Input
-                id="firstname"
-                placeholder="John"
-                type="text"
-                name="firstname"
-                required
-                minLength={2}
-                maxLength={32}
-              />
-            </LabelInputContainer>
-            <LabelInputContainer>
-              <Label htmlFor="lastname">Last name</Label>
-              <Input
-                id="lastname"
-                placeholder="Doe"
-                type="text"
-                name="lastname"
-                required
-                minLength={2}
-                maxLength={32}
-              />
-            </LabelInputContainer>
-          </div>
-          <LabelInputContainer className="mb-4">
-            <Label htmlFor="email">Email Address</Label>
-            <Input
-              id="email"
-              placeholder="you@example.com"
-              type="email"
-              name="email"
-              required
-            />
-          </LabelInputContainer>
-          <LabelInputContainer className="mb-8">
-            <Label htmlFor="password">Password</Label>
-            <Input
-              id="password"
-              placeholder="••••••••"
-              type="password"
-              name="password"
-              required
-              minLength={8}
-              maxLength={16}
-            />
-          </LabelInputContainer>
-
-          <Button
-            className="flex justify-center items-center group/btn relative h-10 w-full rounded-md bg-gradient-to-br from-black to-neutral-600 font-medium text-white shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:bg-zinc-800 dark:from-zinc-900 dark:to-zinc-900 dark:shadow-[0px_1px_0px_0px_#27272a_inset,0px_-1px_0px_0px_#27272a_inset]"
-            type="submit"
-            disabled={isPending}
-          >
-            {isPending ? 
-            <Loader2 className="h-4 w-4 animate-spin" /> :
-             "Sign up"}
-            <BottomGradient />
-          </Button>
-
-          <div className="my-8 flex items-center">
-            <div className="h-[1px] flex-1 bg-gradient-to-r from-transparent via-neutral-300 to-transparent dark:via-neutral-700" />
-            <span className="mx-4 text-sm text-neutral-600 dark:text-neutral-400">
-              or continue with
-            </span>
-            <div className="h-[1px] flex-1 bg-gradient-to-r from-transparent via-neutral-300 to-transparent dark:via-neutral-700" />
-          </div>
-
-          <div className="flex flex-col space-y-4">
-            <button
-              className="group/btn shadow-input relative flex h-10 w-full items-center justify-center space-x-2 rounded-md bg-gray-50 px-4 font-medium text-black dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_#262626]"
-              type="button"
-              onClick={() => Oauth("discord", { redirectTo: '/dashboard' })}
-            >
-              <AiFillDiscord className="h-5 w-5" />
-              <span className="text-sm text-neutral-700 dark:text-neutral-300">
-                Discord
-              </span>
-              <BottomGradient />
-            </button>
-            <button
-              className="group/btn shadow-input relative flex h-10 w-full items-center justify-center space-x-2 rounded-md bg-gray-50 px-4 font-medium text-black dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_#262626]"
-              type="button"
-              onClick={async () => await Oauth("google", { redirectTo: '/dashboard' })}
-            >
-              <GoogleIcon className="h-5 w-5" />
-              <span className="text-sm text-neutral-700 dark:text-neutral-300">
-                Google
-              </span>
-              <BottomGradient />
-            </button>
-          </div>
-
-          <p className="mt-6 text-center text-sm text-neutral-600 dark:text-neutral-400">
-            Already have an account?{" "}
-            <Link
-              href="/login"
-              className="font-medium text-neutral-800 underline dark:text-neutral-200"
-            >
-              Sign in
-            </Link>
+    return (
+      <div className="flex min-h-screen items-center justify-center bg-gray-50 px-4 py-12 dark:bg-zinc-900">
+        <ToastContainer />
+        <div className="shadow-input mx-auto w-full max-w-md rounded-2xl bg-white p-8 dark:bg-black">
+          <h2 className="text-2xl font-bold text-neutral-800 dark:text-neutral-200">
+            Create an account
+          </h2>
+          <p className="mt-2 max-w-sm text-sm text-neutral-600 dark:text-neutral-300">
+            Join us today and start your journey with our platform.
           </p>
-        </form>
-      </div>
-    </div>
-  );
-}
 
+          <form action={formAction} className="my-8">
+            <div className="mb-4 flex flex-col space-y-2 md:flex-row md:space-y-0 md:space-x-2">
+              <LabelInputContainer>
+                <Label htmlFor="firstname">First name</Label>
+                <Input
+                  id="firstname"
+                  placeholder="John"
+                  type="text"
+                  name="firstname"
+                  required
+                  minLength={2}
+                  maxLength={32}
+                />
+              </LabelInputContainer>
+              <LabelInputContainer>
+                <Label htmlFor="lastname">Last name</Label>
+                <Input
+                  id="lastname"
+                  placeholder="Doe"
+                  type="text"
+                  name="lastname"
+                  required
+                  minLength={2}
+                  maxLength={32}
+                />
+              </LabelInputContainer>
+            </div>
+            <LabelInputContainer className="mb-4">
+              <Label htmlFor="email">Email Address</Label>
+              <Input
+                id="email"
+                placeholder="you@example.com"
+                type="email"
+                name="email"
+                required
+              />
+            </LabelInputContainer>
+            <LabelInputContainer className="mb-8">
+              <Label htmlFor="password">Password</Label>
+              <Input
+                id="password"
+                placeholder="••••••••"
+                type="password"
+                name="password"
+                required
+                minLength={8}
+                maxLength={16}
+              />
+            </LabelInputContainer>
+
+            <Button
+              className="flex justify-center items-center group/btn relative h-10 w-full rounded-md bg-gradient-to-br from-black to-neutral-600 font-medium text-white shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:bg-zinc-800 dark:from-zinc-900 dark:to-zinc-900 dark:shadow-[0px_1px_0px_0px_#27272a_inset,0px_-1px_0px_0px_#27272a_inset]"
+              type="submit"
+              disabled={isPending}
+            >
+              {isPending ? 
+              <Loader2 className="h-4 w-4 animate-spin" /> :
+              "Sign up"}
+              <BottomGradient />
+            </Button>
+
+            <div className="my-8 flex items-center">
+              <div className="h-[1px] flex-1 bg-gradient-to-r from-transparent via-neutral-300 to-transparent dark:via-neutral-700" />
+              <span className="mx-4 text-sm text-neutral-600 dark:text-neutral-400">
+                or continue with
+              </span>
+              <div className="h-[1px] flex-1 bg-gradient-to-r from-transparent via-neutral-300 to-transparent dark:via-neutral-700" />
+            </div>
+
+            <div className="flex flex-col space-y-4">
+              <button
+                className="group/btn shadow-input relative flex h-10 w-full items-center justify-center space-x-2 rounded-md bg-gray-50 px-4 font-medium text-black dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_#262626]"
+                type="button"
+                disabled={dpending}
+                onClick={async () => {
+                  setdpending(true);
+                  await Oauth("discord", { redirectTo: '/dashboard' })}
+                  }
+                  >
+                {dpending ? 
+                  <>
+                  <AiFillDiscord className="h-5 w-5 animate-spin" />
+                  <span className="text-sm text-neutral-700 dark:text-neutral-300">
+                    Discord
+                  </span>
+                  </> :
+                  <>
+                  <AiFillDiscord className="h-5 w-5" />
+                  <span className="text-sm text-neutral-700 dark:text-neutral-300">
+                    Discord
+                  </span>
+                  </>
+                  }
+                <BottomGradient />
+              </button>
+              <button
+                className="group/btn shadow-input relative flex h-10 w-full items-center justify-center space-x-2 rounded-md bg-gray-50 px-4 font-medium text-black dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_#262626]"
+                type="button"
+                disabled={gpending}
+                onClick={
+                    async () => {
+                    setgpending(true);
+                    await Oauth("google", { redirectTo: '/dashboard' })}
+                  }
+                    
+              >
+                {/* <GoogleIcon className="h-5 w-5" /> */}
+                  {gpending ? 
+                  <>
+                  <GoogleIcon className="h-5 w-5 animate-spin" />
+                  <span className="text-sm text-neutral-700 dark:text-neutral-300">
+                    Google
+                  </span>
+                  </> :
+                  <>
+                  <GoogleIcon className="h-5 w-5" />
+                  <span className="text-sm text-neutral-700 dark:text-neutral-300">
+                    Google
+                  </span>
+                  </>
+                  }
+                <BottomGradient />
+              </button>
+            </div>
+
+            <p className="mt-6 text-center text-sm text-neutral-600 dark:text-neutral-400">
+              Already have an account?{" "}
+              <Link
+                href="/login"
+                className="font-medium text-neutral-800 underline dark:text-neutral-200"
+              >
+                Sign in
+              </Link>
+            </p>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
 const BottomGradient = () => {
   return (
     <>
