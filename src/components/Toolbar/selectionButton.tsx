e168ba91ec9e9ad54b2ac6cec876ec9a2cd2fa71
Saket Singh <137860573+Saket6198@users.noreply.github.com>
2025-04-30 21:54:25 +0530

    WIP: dropdown shape seletion button for toolbar

---
+++ src/components/Toolbar/selectionButton.tsx
@@ -0,0 +1,19 @@
+"use client";
+import { CanvasMode } from '../../types';
+export default function SelectionButton ({
+    isActive,
+    canvasMode,
+    onClick,
+}: {
+    isActive: boolean;
+    canvasMode: CanvasMode;
+    onClick: (newState: CanvasMode) => void;
+
+}){
+    return (
+        
+        <div>
+
+        </div>
+    );
+}
