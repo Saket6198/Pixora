b32d0d85164f8149bc7b99219a5ec77c67402654
Saket Singh <137860573+Saket6198@users.noreply.github.com>
2025-05-03 18:15:36 +0530

    correct button className for consistent styling

--- src/components/Toolbar/iconbutton.tsx
+++ src/components/Toolbar/iconbutton.tsx
@@ -13,7 +13,7 @@ export default function IconButton({
     <button
       onClick={onClick}
       disabled={disabled}
-      className={`${isActive ? "bg-gray-100 text-blue-600 hover:enabled: text-blue-600 focus:enabled: text-blue-600 active:enabled: text-blue-600" : ""} focus:enabled active:enabled: disabled:cursor-default disabled: flex min-h-[128px] min-w-[28px] items-center justify-center rounded-md text-gray-500 text-gray-700 text-gray-900 opacity-60 hover:enabled:text-gray-700`}
+      className={`${isActive ? "bg-gray-100 text-blue-600 hover:enabled:text-blue-600 focus:enabled:text-blue-600 active:enabled:text-blue-600" : ""} disabled:cursor-default flex min-h-[28px] min-w-[28px] items-center justify-center rounded-md text-gray-500 hover:enabled:text-gray-700 focus:enabled:text-gray-700 active:enabled:text-gray-900 opacity-60`}
     >
       {children}
     </button>
