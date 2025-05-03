420259c812e32722ea873914aca266c7f4c31f9d
Saket Singh <137860573+Saket6198@users.noreply.github.com>
2025-05-03 18:16:01 +0530

    refactor Toolbar component for improved structure and readability

--- src/components/Toolbar/toolbar.tsx
+++ src/components/Toolbar/toolbar.tsx
@@ -1,18 +1,41 @@
-import type { CanvasState } from "~/types";
+import { CanvasMode, LayerType, type CanvasState } from "~/types";
+import SelectionButton from "./selectionButton";
+import ShapeSelectionButton from "./ShapesselectionButton";
+
 
 export default function Toolbar({
-    canvasState,
-    setCanvasState
-} : {
-    canvasState: CanvasState,
-    setCanvasState: (newState: CanvasState) => void
-    }
-) {
-    return (
-         <div className="fixed bottom-4 left-1/2 z-10 flex items-center justify-center rounded-lg bg-white p-1 shadow-[0_0_3px_rgba(0,0,0,18)]">
-            <div className="flex justify-center items-center gap-3">
-                
-            </div>
-         </div>
-    );
+  canvasState,
+  setCanvasState,
+}: {
+  canvasState: CanvasState;
+  setCanvasState: (newState: CanvasState) => void;
+}) {
+  return (
+    <div className="fixed bottom-4 left-1/2 z-10 flex -translate-x-1/2 items-center justify-center rounded-lg bg-white p-1 shadow-[0_0_3px_rgba(0,0,0,0.18)]">
+      <div className="flex items-center justify-center gap-3">
+        <SelectionButton
+          isActive={canvasState.mode === CanvasMode.None || canvasState.mode === CanvasMode.Dragging}
+          canvasMode={canvasState.mode}
+          onClick={(canvasMode) =>
+            setCanvasState(
+              canvasMode === CanvasMode.Dragging
+                ? { mode: CanvasMode.Dragging, origin: null }
+                : { mode: CanvasMode.None },
+            )
+          }
+        />
+        <ShapeSelectionButton 
+            isActive={canvasState.mode === CanvasMode.inserting && 
+                [LayerType.Rectangle, LayerType.Ellipse].includes(canvasState.layerType)
+            }
+            canvasState={canvasState}
+            onClick={(layerType) =>
+            setCanvasState({
+                mode: CanvasMode.inserting,
+                layerType
+            })
+        }></ShapeSelectionButton>
+      </div>
+    </div>
+  );
 }
