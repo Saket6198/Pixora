97d64c10ebc98ac7b2928d84403e89deab16fd0c
Saket Singh <137860573+Saket6198@users.noreply.github.com>
2025-05-05 22:08:08 +0530

    zoom icons in toolbar

--- src/components/Toolbar/toolbar.tsx
+++ src/components/Toolbar/toolbar.tsx
@@ -1,20 +1,32 @@
 import { CanvasMode, LayerType, type CanvasState } from "~/types";
 import SelectionButton from "./selectionButton";
 import ShapeSelectionButton from "./ShapesselectionButton";
-
+import ZoomInButton from "./zoomInButton";
+import ZoomOutButton from "./zoomOutButton";
 
 export default function Toolbar({
   canvasState,
   setCanvasState,
+  zoomIn,
+  zoomOut,
+  canZoomIn,
+  canZoomOut,
 }: {
   canvasState: CanvasState;
   setCanvasState: (newState: CanvasState) => void;
+  zoomIn: () => void;
+  zoomOut: () => void;
+  canZoomIn: boolean;
+  canZoomOut: boolean;
 }) {
   return (
     <div className="fixed bottom-4 left-1/2 z-10 flex -translate-x-1/2 items-center justify-center rounded-lg bg-white p-1 shadow-[0_0_3px_rgba(0,0,0,0.18)]">
       <div className="flex items-center justify-center gap-3">
         <SelectionButton
-          isActive={canvasState.mode === CanvasMode.None || canvasState.mode === CanvasMode.Dragging}
+          isActive={
+            canvasState.mode === CanvasMode.None ||
+            canvasState.mode === CanvasMode.Dragging
+          }
           canvasMode={canvasState.mode}
           onClick={(canvasMode) =>
             setCanvasState(
@@ -24,17 +36,28 @@ export default function Toolbar({
             )
           }
         />
-        <ShapeSelectionButton 
-            isActive={canvasState.mode === CanvasMode.inserting && 
-                [LayerType.Rectangle, LayerType.Ellipse].includes(canvasState.layerType)
-            }
-            canvasState={canvasState}
-            onClick={(layerType) =>
+        <ShapeSelectionButton
+          isActive={
+            canvasState.mode === CanvasMode.inserting &&
+            [LayerType.Rectangle, LayerType.Ellipse].includes(
+              canvasState.layerType,
+            )
+          }
+          canvasState={canvasState}
+          onClick={(layerType) =>
             setCanvasState({
-                mode: CanvasMode.inserting,
-                layerType
+              mode: CanvasMode.inserting,
+              layerType,
             })
-        }></ShapeSelectionButton>
+          }
+        ></ShapeSelectionButton>
+        <div className="w-[1px] bg-black/10 self-stretch" />
+        <div className="flex items-center justify-center">
+            <ZoomInButton onClick={zoomIn} disabled={!canZoomIn} />
+            <ZoomOutButton onClick={zoomOut} disabled={!canZoomOut} />
+
+            
+        </div>
       </div>
     </div>
   );
